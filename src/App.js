4bcc76ade5b84426a74aaf418d790d248e4664a6
27ManishaPatel <manisha270989@gmail.com>
2023-07-03 23:43:04 +0200

    stored the value in states

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-export default function Square() {
+
+import React, {useState} from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    setValue('X')
+  }
+  return (
+    <button onClick={handleClick} className="square">{value}</button>
+  )
+}
+
+export default function Board() {
   return (
     <div>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square  />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square  />
+        <Square  />
       </div>
     </div>
   );
